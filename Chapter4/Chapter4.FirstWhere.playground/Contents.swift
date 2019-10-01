473352075774e0cecbb691b698a5a4b603b89995
Alberto Penas Amor <albertopeam@gmail.com>
2019-10-01 23:17:23 +0200

    updated Chapter4.FirstWhere

--- Chapter4/Chapter4.FirstWhere.playground/Contents.swift
+++ Chapter4/Chapter4.FirstWhere.playground/Contents.swift
@@ -8,3 +8,18 @@ var subscription = Set<AnyCancellable>()
     .sink(receiveCompletion: { print("Completed with: \($0)") },
           receiveValue: { print($0) })
     .store(in: &subscription)
+
+// First where will finish the subscription if the condition is true for any of the values sended
+let publisher = PassthroughSubject<Int, Never>()
+publisher.print()
+    .first(where: { $0 % 2 == 0 })
+    .sink(receiveCompletion: { print("Completed with: \($0)") },
+          receiveValue: { print($0) })
+    .store(in: &subscription)
+publisher.send(1)
+publisher.send(3)
+publisher.send(2)
+publisher.send(2)
+publisher.send(3)
+
+// publisher.send(completion: .finished) // not needed if any value makes the predicate true
