dfda3377fb34fdcbed5852011354ae7a256aa32f
Alberto Penas Amor <albertopeam@gmail.com>
2019-10-20 14:46:46 +0200

    added Chapter6.Collect+TimeLine part2

--- Chapter6/Chapter6.Collect+TimeLine.playground/Contents.swift
+++ Chapter6/Chapter6.Collect+TimeLine.playground/Contents.swift
@@ -5,25 +5,31 @@ import PlaygroundSupport
 
 let publishEvery = 1.0
 let collectTimeStride = 4
+let collectMaxCount = 2
 
 let publisher = PassthroughSubject<Date, Never>()
 let delayedPublisher = publisher
     .collect(.byTime(DispatchQueue.main, .seconds(collectTimeStride)))
+    .flatMap({ $0.publisher })
+let delayedPublisherByTimeOrCount = publisher
+    .collect(.byTimeOrCount(DispatchQueue.main, .seconds(collectTimeStride), collectMaxCount))
     .flatMap({ $0.publisher })
 
 let timer = Timer.publish(every: publishEvery, on: RunLoop.main, in: .common)
     .autoconnect()
     .subscribe(publisher)
 
-
 let events = TimelineView(title: "Events")
 let delayedEvents = TimelineView(title: "Collect Events every \(collectTimeStride) secs")
+let delayedEventsByTimeOrCount = TimelineView(title: "Collect Events every \(collectTimeStride) secs or every \(collectMaxCount) events")
 
-let rootView = VStack(spacing: 40) {
+let rootView = VStack(spacing: 30) {
     events
     delayedEvents
+    delayedEventsByTimeOrCount
 }
 PlaygroundPage.current.liveView = UIHostingController(rootView: rootView)
 
 publisher.displayEvents(in: events)
 delayedPublisher.displayEvents(in: delayedEvents)
+delayedPublisherByTimeOrCount.displayEvents(in: delayedEventsByTimeOrCount)
