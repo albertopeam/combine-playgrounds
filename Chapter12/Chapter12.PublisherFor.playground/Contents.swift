58914beb65f0f7fe887f817105fc8b5b86b9b274
Alberto Penas Amor <albertopeam@gmail.com>
2019-10-31 16:17:03 +0100

    updated

--- Chapter12/Chapter12.PublisherFor.playground/Contents.swift
+++ Chapter12/Chapter12.PublisherFor.playground/Contents.swift
@@ -12,21 +12,3 @@ queue.publisher(for: \.operationCount) // KVO observing using publisher
 queue.addOperation {
     print("Added Op.")
 }
-
-// classic KVO property observing
-class Stuff: NSObject {
-    @objc dynamic var property: String
-    
-    init(property: String) {
-        self.property = property
-    }
-}
-
-let someStuff = Stuff(property: "!")
-someStuff.property = "Hello!"
-someStuff
-    .publisher(for: \.property)
-    .print()
-    .sink(receiveValue: { print($0) })
-    .store(in: &subscriptions)
-someStuff.property = "Hello World!"
