cfd42fc25dcef648d667c82125ab44a55ecf8f60
Alberto Penas Amor <albertopeam@gmail.com>
2019-11-25 22:33:54 +0100

    added RunLoop.schedule

--- Chapter17/Chapter17.RunLoop.playground/Contents.swift
+++ Chapter17/Chapter17.RunLoop.playground/Contents.swift
@@ -8,15 +8,24 @@ let source =
     .autoconnect()
     .scan(0, { previous, _ in previous + 1 })
 
-let setupPublisher = { recorder in
-    source
+var threadRecorder: ThreadRecorder? = nil
+
+let setupPublisher = { (recorder: ThreadRecorder) in
+    return source
         .receive(on: DispatchQueue.global()) // it will receive in one of the available threads of the global pool
         //.subscribe(on: DispatchQueue.global()) // as source is emitting on main thread, then we´ll receive from main
         .recordThread(using: recorder) // records in which thread is being emitted
         .receive(on: RunLoop.current) // thread that is being used when invoking this call, this case is main
         .recordThread(using: recorder) // records in which thread is being emitted after the receive(on)
+        .handleEvents(receiveSubscription: { _ in threadRecorder = recorder })
         .eraseToAnyPublisher()
 }
+
+RunLoop.current
+    .schedule(after: .init(Date(timeIntervalSinceNow: 5)),
+              tolerance: .milliseconds(500)) {
+                threadRecorder?.subscription?.cancel() // do cancel from RunLoop.current
+    }
 
 let view = ThreadRecorderView(title: "Using ImmediateScheduler", setup: setupPublisher)
 PlaygroundPage.current.liveView = UIHostingController(rootView: view)
